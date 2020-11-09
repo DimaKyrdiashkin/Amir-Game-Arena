903cc8ae433ffaa4abf70d5809b7b4d09823095c
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-11-09 13:08:52 +0200

    start

---
+++ gulp/tasks/pug.js
@@ -0,0 +1,7 @@
+module.exports = ()=>{
+    $.gulp.task('pug', ()=>{
+        return $.gulp.src('src/pug/pages/*.html')
+            .pipe($.gulp.dest('build'))
+            .on('end', $.bs.reload);
+    })
+}
---
+++ src/static/js/main.js
@@ -0,0 +1,57 @@
+const card =  document.querySelectorAll('.cardT');
+const cardBottom = document.querySelectorAll('#card_bottom');
+const cardHeight = [];
+const minH = [];
+
+card.forEach((item)=>{
+    cardHeight.push(item.clientHeight + 2)
+});
+const button = document.querySelectorAll('.card_button');
+
+const open=(i)=>{
+    if(button[i].className.split(' ')[1] == 'open'){
+        cardBottom[i].classList.remove("open");
+        button[i].classList.remove("open");
+        return false
+    }else{
+        cardBottom.forEach((item, i)=>{
+            item.classList.remove('open');
+            button[i].classList.remove('open');
+        })
+        cardBottom[i].classList.add('open');
+    }
+    button[i].classList.add("open");
+}
+
+button.forEach((item, i)=>{
+    item.addEventListener("click", ()=>{open(i)});
+});
+
+
+
+// menu
+const mBut = document.querySelector('.mobButton');
+const menu = document.querySelector('.mobMenu');
+const mobButton = document.querySelectorAll('.mobItems');
+
+mBut.addEventListener("click", ()=>{
+    menu.classList.toggle('open');
+    mBut.classList.toggle('open');
+});
+
+mobButton.forEach((item, i)=>{
+    item.addEventListener("click", ()=>{
+        menu.classList.remove('open');
+        mBut.classList.remove('open');
+    })
+});
+
+const wrap = document.querySelector(".wrapper");
+
+window.addEventListener('scroll', ()=>{  
+    if(pageYOffset-250>document.documentElement.clientHeight){
+        wrap.classList.add("scroll");
+    }else{
+        wrap.classList.remove("scroll");
+    }
+})
