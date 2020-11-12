d180b4e537315854181890b44c6e8e3bc445379a
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-11-12 18:04:05 +0200

    12.11.2020

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,6 +1,6 @@
 module.exports =()=>{
     $.gulp.task('scripts:lib', ()=>{
-        return $.gulp.src(['node_modules/jquery/dist/jquery.min.js',])// библиотеки
+        return $.gulp.src(['node_modules/jquery/dist/jquery.min.js','src/static/js/owl.carousel.min.js','src/static/js/wow.min.js'])// библиотеки
             .pipe($.gp.concat('libs.min.js'))
             .pipe($.gulp.dest('build/static/js/'))
             .pipe($.bs.reload({
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,57 +1,25 @@
-const card =  document.querySelectorAll('.cardT');
-const cardBottom = document.querySelectorAll('#card_bottom');
-const cardHeight = [];
-const minH = [];
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,  
+    center: true,
+    margin:30,
+    autoplay:true,
+    autoplayTimeout:50000000,
+    autoplayHoverPause:true,
+    responsive:{
+        600:{
+            items:2,
+        }
+    },
+  });
 
-card.forEach((item)=>{
-    cardHeight.push(item.clientHeight + 2)
-});
-const button = document.querySelectorAll('.card_button');
-
-const open=(i)=>{
-    if(button[i].className.split(' ')[1] == 'open'){
-        cardBottom[i].classList.remove("open");
-        button[i].classList.remove("open");
-        return false
-    }else{
-        cardBottom.forEach((item, i)=>{
-            item.classList.remove('open');
-            button[i].classList.remove('open');
-        })
-        cardBottom[i].classList.add('open');
-    }
-    button[i].classList.add("open");
-}
-
-button.forEach((item, i)=>{
-    item.addEventListener("click", ()=>{open(i)});
-});
-
-
-
-// menu
-const mBut = document.querySelector('.mobButton');
-const menu = document.querySelector('.mobMenu');
-const mobButton = document.querySelectorAll('.mobItems');
-
-mBut.addEventListener("click", ()=>{
-    menu.classList.toggle('open');
-    mBut.classList.toggle('open');
-});
-
-mobButton.forEach((item, i)=>{
-    item.addEventListener("click", ()=>{
-        menu.classList.remove('open');
-        mBut.classList.remove('open');
+  
+  //form
+  const form = document.querySelector(".contact_form");
+  const filds = [form.name, form.mail, form.message];
+  filds.forEach((item)=>{
+    item.addEventListener('change', ()=>{
+      item.value != '' ? item.parentElement.classList.add('active') : item.parentElement.classList.remove('active')
     })
+  });
 });
-
-const wrap = document.querySelector(".wrapper");
-
-window.addEventListener('scroll', ()=>{  
-    if(pageYOffset-250>document.documentElement.clientHeight){
-        wrap.classList.add("scroll");
-    }else{
-        wrap.classList.remove("scroll");
-    }
-})
